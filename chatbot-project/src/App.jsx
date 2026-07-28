1fabbe6330527779fd0b0930cf66769c6540b870
jabin <khadka.jabin10@gmail.com>
2026-07-28 21:03:04 +0545

    we separate our code into diffrent files

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,35 +1,141 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef, useEffect } from "react";
+import { chatbot } from "supersimpledev";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+import "./App.css";
+import RobotProfileImage from "./assets/chatbot.jpg";
+import UserProfileImage from "./assets/man.jpg";
+function ChatInput({ chatMessages, setChatMessages }) {
+  const [inputText, setInputText] = useState();
+  function saveInputText(event) {
+    setInputText(event.target.value); //Here you're calling the function setInputText and passing it a value. you are not assing to setinputtext
+  }
+  function sendMessage() {
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: "user",
+        id: crypto.randomUUID(),
+      },
+    ];
+    setChatMessages(newChatMessages);
+    const response = chatbot.getResponse(inputText);
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: response,
+        sender: "robot",
+        id: crypto.randomUUID(),
+      },
+    ]);
+    setInputText("");
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="chat-input-container">
+      <input
+        placeholder="Send a message to Chatbot"
+        size="30"
+        onChange={saveInputText}
+        value={inputText}
+        className="input"
+      />
+      <button onClick={sendMessage} className="send-button">
+        Send
+      </button>
+    </div>
+  );
+}
+function ChatMessage({ message, sender }) {
+  //this or this ChatMessage(props ) then  const message = props.message;
+  //const message = props.message;
+  //const sender = props.sender;
+
+  if (sender === "robot") {
+    return (
+      <div className="chat-robot">
+        <img
+          src={RobotProfileImage}
+          width="50"
+          height="50"
+          style={{ borderRadius: "50%" }}
+        />
+        <div class="message">{message}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    );
+  } else {
+    return (
+      <div className="chat-user">
+        <div class="message">{message}</div>
+
+        <img
+          src={UserProfileImage}
+          width="50"
+          height="50"
+          style={{ borderRadius: "50%" }}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
+}
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useRef(null);
+  useEffect(() => {
+    const containerElem = chatMessagesRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, [chatMessages]);
+  return (
+    <div className="chat-messages-container" ref={chatMessagesRef}>
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            key={chatMessage.id}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+function App() {
+  const array = useState([
+    {
+      message: "hello Chatbot",
+      sender: "user",
+      id: "id1",
+    },
+    {
+      message: "Hello! How can I help you",
+      sender: "robot",
+      id: "id2",
+    },
+    {
+      message: "can you get mme todays date?",
+      sender: "user",
+      id: "id3",
+    },
+    {
+      message: "today is sept 27",
+      sender: "robot",
+      id: "id4",
+    },
+  ]);
+
+  const chatMessages = array[0]; //shorcut const [chatMessage,setChatMessages]=array; orders matter ok ,
+  const setChatMessages = array[1];
+
+  return (
+    <div className="app-container">
+      <ChatMessages chatMessages={chatMessages} />
+      <ChatInput
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
+  );
+}
+
+export default App;
